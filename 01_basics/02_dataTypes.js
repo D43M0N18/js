6d7f17b34c6b1404e74901d7f970ef46f3a936a7
S4CH4L <134863317+D43M0N18@users.noreply.github.com>
2024-07-31 17:54:52 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use Strict"; // treat all JS code as newer version
+
+// alert (3 + 3) // we are using nod.js not browser
+
+let name = "Sachal"
+let age = 18
+let isLoggedIn = false
+
+//number => 2^53
+// bigint
+// string => ""
+// boolean
+// null
+// undefined =>
+// symbol
+
+
+// object
+
+console.log(typeof "Sachal")
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
